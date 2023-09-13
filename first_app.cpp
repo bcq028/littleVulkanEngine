cb0c634450251af363398698883ac2ecf75c686e
bcq <bcq028@outlook.com>
2023-09-13 22:51:02 +0800

    compile shader error try windows

--- first_app.cpp
+++ first_app.cpp
@@ -50,8 +50,8 @@ namespace lve
         pipelineConfig.pipelineLayout = pipelineLayout;
         lvePipeline = std::make_unique<LvePipeline>(
             lveDevice,
-            "../shaders/simple_shader.vert.spv",
-            "../shaders/simple_shader.frag.spv",
+            "/home/moona/littleVulkanEngine/shaders/simple_shader.vert.spv",
+            "/home/moona/littleVulkanEngine/shaders/simple_shader.frag.spv",
             pipelineConfig);
     }
 
